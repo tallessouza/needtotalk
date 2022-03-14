3cf3ed9bd98b28eedb40ac8322b8da10b2a03be3
tallessouza <talles-swd@live.com>
2022-03-14 12:22:49 -0300

    Modal logic

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -2,17 +2,29 @@ import Modal from './modal.js'
 
 const modal = Modal()
 
+const modalTitle = document.querySelector('.modal h2')
+const modalDescription = document.querySelector('.modal p')
+const modalButton = document.querySelector('.modal button')
+
 const checkButtons = document.querySelectorAll('.actions a.check')
 const deleteButton = document.querySelectorAll('.actions a.delete')
 
 checkButtons.forEach(button => {
-  button.addEventListener('click', event => {
-    modal.open()
-  })
+  button.addEventListener('click', handleClick)
 })
 
 deleteButton.forEach(button => {
-  button.addEventListener('click', event => {
-    modal.open()
-  })
+  button.addEventListener('click', event => handleClick(event, false))
 })
+
+function handleClick(event, check = true) {
+  event.preventDefault()
+  const text = check ? 'Marcar como lida ' : 'Excluir '
+  modalTitle.innerHTML = `${text}essa pergunta`
+  modalDescription.innerHTML = check
+    ? `Tem certeza que deseja ${text.toLowerCase()} esta pergunta?`
+    : `Tem certeza que deseja ${text.toLowerCase()} esta pergunta?`
+  modalButton.innerHTML = `Sim, ${text.toLowerCase()}`
+  check ? modalButton.classList.remove('red') : modalButton.classList.add('red')
+  modal.open()
+}
