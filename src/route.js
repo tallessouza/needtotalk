b624ce4701659b0fa111413af4d97592d10d9ff5
tallessouza <talles-swd@live.com>
2022-03-14 22:07:40 -0300

    Room routing creation and path fixed

--- src/route.js
+++ src/route.js
@@ -1,5 +1,6 @@
 const express = require('express')
-const questionController = require('./controllers/QuestionController')
+const QuestionController = require('./controllers/QuestionController')
+const RoomController = require('./controllers/RoomController')
 
 const route = express.Router()
 
@@ -7,7 +8,8 @@ route.get('/', (req, res) => res.render('index', { page: 'enter-room' }))
 route.get('/create-pass', (req, res) =>
   res.render('index', { page: 'create-pass' })
 )
-route.get('/room', (req, res) => res.render('room'))
-route.post('/room/:room/:question/:action', questionController.index)
+route.get('/room/:room', (req, res) => res.render('room'))
+route.post('/room/:room/:question/:action', QuestionController.index)
+route.post('/room/create-room', RoomController.create)
 
 module.exports = route
