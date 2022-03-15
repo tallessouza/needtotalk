7fa0c0ac1cc95eea695bb053a3b6b74bf1e19815
tallessouza <talles-swd@live.com>
2022-03-14 22:52:31 -0300

    For mistake and ident

--- src/controllers/RoomController.js
+++ src/controllers/RoomController.js
@@ -4,18 +4,19 @@ module.exports = {
   async create(req, res) {
     const db = await Database()
     const pass = req.body.password
-    let roomId 
+    let roomId
 
-    for(var i=0; i < 6. i++) {
-      i ==0 ? Math.floor(Math.random() * 10).toString():
-      roomId += Math.floor(Math.random() * 10).toString()
+    for (var i = 0; i < 6; i++) {
+      i == 0
+        ? Math.floor(Math.random() * 10).toString()
+        : (roomId += Math.floor(Math.random() * 10).toString())
     }
 
     await db.run(`INSERT INTO rooms (
       id,
       pass
     ) VALUES (
-      ${(parseInt(roomId))},
+      ${parseInt(roomId)},
       ${pass}
     )`)
 
