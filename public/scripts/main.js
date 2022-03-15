f76c5f2762363c205d6e9c46084f6827f1c575cf
tallessouza <talles-swd@live.com>
2022-03-15 19:31:58 -0300

    Project rush

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -6,12 +6,16 @@ const modalTitle = document.querySelector('.modal h2')
 const modalDescription = document.querySelector('.modal p')
 const modalButton = document.querySelector('.modal button')
 
+//Pegar todos os botões que existe com a classe check
 const checkButtons = document.querySelectorAll('.actions a.check')
-const deleteButton = document.querySelectorAll('.actions a.delete')
 
 checkButtons.forEach(button => {
+  //adicionar a escuta
   button.addEventListener('click', handleClick)
 })
+
+/*Quando o botão delete for clicado ele abre a modal */
+const deleteButton = document.querySelectorAll('.actions a.delete')
 
 deleteButton.forEach(button => {
   button.addEventListener('click', event => handleClick(event, false))
@@ -19,7 +23,7 @@ deleteButton.forEach(button => {
 
 function handleClick(event, check = true) {
   event.preventDefault()
-  const text = check ? 'Marcar como lida ' : 'Excluir '
+  const text = check ? 'Marcar como lida' : 'Excluir'
   const slug = check ? 'check' : 'delete'
   const roomId = document.querySelector('#room-id').dataset.id
   const questionId = event.target.dataset.id
@@ -27,14 +31,11 @@ function handleClick(event, check = true) {
   const form = document.querySelector('.modal form')
   form.setAttribute('action', `/question/${roomId}/${questionId}/${slug}`)
 
-  modalTitle.innerHTML = `${text}essa pergunta`
-
-  modalDescription.innerHTML = check
-    ? `Tem certeza que deseja ${text.toLowerCase()} esta pergunta?`
-    : `Tem certeza que deseja ${text.toLowerCase()} esta pergunta?`
-
+  modalTitle.innerHTML = `${text} esta pergunta`
+  modalDescription.innerHTML = `Tem certeza que deseja ${text.toLowerCase()} esta pergunta?`
   modalButton.innerHTML = `Sim, ${text.toLowerCase()}`
   check ? modalButton.classList.remove('red') : modalButton.classList.add('red')
 
+  //abrir modal
   modal.open()
 }
--- src/db/init.js
+++ src/db/init.js
@@ -5,15 +5,16 @@ const initDb = {
     const db = await Database()
 
     await db.exec(`CREATE TABLE rooms (
-      id INTEGER PRIMARY KEY,
-      pass TEXT
-    )`)
+            id INTEGER PRIMARY KEY,
+            pass TEXT
+        )`)
 
     await db.exec(`CREATE TABLE questions (
-      id INTEGER PRIMARY KEY AUTOINCREMENT,
-      title TEXT,
-      read INT
-    )`)
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            title TEXT,
+            read INT,
+            room INT
+        )`)
 
     await db.close()
   }
