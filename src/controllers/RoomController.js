c8e27d72eb4ef2d659ac32c10794decf27ba9e7c
tallessouza <talles-swd@live.com>
2022-03-14 22:51:25 -0300

    Insert data on room table

--- src/controllers/RoomController.js
+++ src/controllers/RoomController.js
@@ -1,6 +1,26 @@
+const Database = requier('../db/config')
+
 module.exports = {
-  create(req, res) {
-    let roomId = 12345
+  async create(req, res) {
+    const db = await Database()
+    const pass = req.body.password
+    let roomId 
+
+    for(var i=0; i < 6. i++) {
+      i ==0 ? Math.floor(Math.random() * 10).toString():
+      roomId += Math.floor(Math.random() * 10).toString()
+    }
+
+    await db.run(`INSERT INTO rooms (
+      id,
+      pass
+    ) VALUES (
+      ${(parseInt(roomId))},
+      ${pass}
+    )`)
+
+    await db.close()
+
     res.redirect(`/room/${roomId}`)
   }
 }
